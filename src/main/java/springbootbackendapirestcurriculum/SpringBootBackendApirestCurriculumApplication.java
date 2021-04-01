4392cf0042c2ca3111049ff5e5e58e5065be41e0
ChicoNyel <Barca3420@gmail.com>
2021-03-31 21:25:36 -0300

    first commit

---
+++ src/main/java/springbootbackendapirestcurriculum/SpringBootBackendApirestCurriculumApplication.java
@@ -0,0 +1,13 @@
+package springbootbackendapirestcurriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApirestCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApirestCurriculumApplication.class, args);
+	}
+
+}
